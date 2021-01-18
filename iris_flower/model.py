8d2791f2141bbd7b250215653c0f4aeb794c74eb
Sayon Sivakumaran <sayon.sivakumaran@gmail.com>
2021-01-18 09:32:06 -0500

    update code for iris flower

--- iris_flower/model.py
+++ iris_flower/model.py
@@ -2,29 +2,29 @@ import pandas as pd
 from sklearn import linear_model
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
-from sklearn.datasets import load_boston
+from sklearn.datasets import load_iris
 
-boston = load_boston()
+iris = load_iris()
 
-boston_x = pd.DataFrame(boston.data, columns=boston.feature_names)
-boston_y = pd.DataFrame(boston.target)
+iris_x = pd.DataFrame(iris.data, columns=iris.feature_names)
+iris_y = pd.DataFrame(iris.target)
 
 reg = linear_model.LinearRegression()
 
-x_train, x_test, y_train, y_test = train_test_split(boston_x, boston_y,
+x_train, x_test, y_train, y_test = train_test_split(iris_x, iris_y,
                                                     test_size=0.33, random_state=4)
 
 
 reg.fit(x_train, y_train)
 
-boston_pred = reg.predict(x_test)
+iris_pred = reg.predict(x_test)
 
 print("Mean squared error")
-print(mean_squared_error(y_test, boston_pred))
+print(mean_squared_error(y_test, iris_pred))
 
 print("Mean absolute error")
-print(mean_absolute_error(y_test, boston_pred))
+print(mean_absolute_error(y_test, iris_pred))
 
 print("R2 Score")
-print(r2_score(y_test, boston_pred))
+print(r2_score(y_test, iris_pred))
 
