d745c5af664f63004683d67849cc0a0bc231a463
Sayon Sivakumaran <sayon.sivakumaran@gmail.com>
2021-01-25 02:38:22 -0500

    add more parameters and include accuracy score

--- iris_flower/model.py
+++ iris_flower/model.py
@@ -4,7 +4,7 @@ import boto3
 import pandas as pd
 from sklearn import neighbors
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score, accuracy_score
 from sklearn.datasets import load_iris
 import mlflow
 import mlflow.sklearn
@@ -53,8 +53,8 @@ with mlflow.start_run(run_name=f'run-{datetime.now().strftime("%Y%m%d%H%M%S")}')
     mlflow.log_param('weights', weights)
 
     mlflow.log_metric('mean_squared_error', mean_squared_error(y_test, iris_pred))
-    mlflow.log_metric('mean_absolute_error', mean_absolute_error(y_test, iris_pred))
     mlflow.log_metric('r2_score', r2_score(y_test, iris_pred))
+    mlflow.log_metric('accuracy_score', accuracy_score(y_test, iris_pred))
 
     mlflow.sklearn.log_model(model, "iris_model")
 
