befcede870b62267eebe4bb714a898d704fc01fa
Sayon Sivakumaran <sayon.sivakumaran@gmail.com>
2021-01-28 00:24:33 -0500

    add experiment name to logging

--- iris_flower/model.py
+++ iris_flower/model.py
@@ -40,14 +40,12 @@ mlflow.set_tracking_uri(TRACKING_URI)
 n_neighbors = int(os.environ.get('n_neighbors', 5))
 weights = os.environ.get('weights', 'uniform')
 
+mlflow.set_experiment(experiment_name='iris_flower_experiment')
 with mlflow.start_run(run_name=f'run-{datetime.now().strftime("%Y%m%d%H%M%S")}'):
-
-    print(mlflow.get_artifact_uri())
 
     knn = neighbors.KNeighborsClassifier(n_neighbors=n_neighbors, weights=weights)
 
     x_train, x_test, y_train, y_test = train_test_split(iris['data'], iris['target'], random_state=0)
-
 
     model = knn.fit(x_train, y_train)
 
