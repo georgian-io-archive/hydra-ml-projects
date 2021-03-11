ff894d0c38661482cb2a559b98626ec98a71f1ab
Sayon Sivakumaran <sayon.sivakumaran@gmail.com>
2021-03-11 08:54:13 -0500

    add indentation code from script

--- iris_flower/model.py
+++ iris_flower/model.py
@@ -19,13 +19,13 @@ weights = os.environ.get('weights', 'uniform')
 mlflow.set_experiment(experiment_name='iris_flower_experiment')
 with mlflow.start_run(run_name=f'run-{datetime.now().strftime("%Y%m%d%H%M%S")}') as run:
 
-knn = neighbors.KNeighborsClassifier(n_neighbors=n_neighbors, weights=weights)
+    knn = neighbors.KNeighborsClassifier(n_neighbors=n_neighbors, weights=weights)
 
-x_train, x_test, y_train, y_test = train_test_split(iris['data'], iris['target'], random_state=0)
+    x_train, x_test, y_train, y_test = train_test_split(iris['data'], iris['target'], random_state=0)
 
-model = knn.fit(x_train, y_train)
+    model = knn.fit(x_train, y_train)
 
-iris_pred = knn.predict(x_test)
+    iris_pred = knn.predict(x_test)
 
     mlflow.log_param('dataset', 'iris_flower')
     mlflow.log_param('algorithm', 'k_neighbors')
